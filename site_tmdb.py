a2a93204a2e952d17272cc2945a07e19dc9d0856
kihyyo <83176214+kihyyo@users.noreply.github.com>
2025-01-11 21:24:04 +0900

    Update site_tmdb.py

--- site_tmdb.py
+++ site_tmdb.py
@@ -23,50 +23,59 @@ class tmdb(object):
         return difflib.SequenceMatcher(a=cls.remove_special_char(seq1.lower()), b=cls.remove_special_char(seq2.lower())).ratio()
 
     @classmethod
-    def search(cls, keyword, year=None, meta_cache={}):
-
+    def search(cls, keyword, year=None, meta_cache={}, searched_keywords=None):
         tmdb.API_KEY = 'f090bb54758cabf231fb605d3e3e0468'
 
+        if searched_keywords is None:
+            searched_keywords = set()
+        
+        if keyword in searched_keywords:
+            return '' 
+
+        searched_keywords.add(keyword)
+    
         if keyword in meta_cache:
             return meta_cache[keyword]
 
-        if SiteUtil.is_include_hangul(keyword):
-            tmdb_search = tmdbsimple.Search().tv(query=keyword, language='ko', include_adult=True)
-        else:
-            tmdb_search = tmdbsimple.Search().tv(query=keyword, language='en', include_adult=True)
-        tmdb_code = ''
-        if tmdb_search['results'] != []:
-            if year == None:
-                score_list = []
-                for t in tmdb_search['results']:
-                    score_list.append(max(cls.similar(t['name'], keyword), cls.similar(t['original_name'], keyword))) 
-                if max(score_list) > 0.7 :
+        language = 'ko' if SiteUtil.is_include_hangul(keyword) else 'en'
+        tmdb_search = tmdbsimple.Search().tv(query=keyword, language=language, include_adult=True)
+    
+        if tmdb_search['results']:
+            if year is None:
+                score_list = [
+                    max(cls.similar(t['name'], keyword), cls.similar(t['original_name'], keyword))
+                    for t in tmdb_search['results']
+                ]
+                if max(score_list) > 0.7:
                     tmdb_code = tmdb_search['results'][score_list.index(max(score_list))]['id']
                 else:
                     tmdb_code = tmdb_search['results'][0]['id']
             else:
+                tmdb_code = ''
                 for t in tmdb_search['results']:
                     try:
                         tmdb_year = int(t['first_air_date'].split('-')[0])
                     except:
                         tmdb_year = 1900
-
-                    if tmdb_year == int(year) :
+    
+                    if tmdb_year == int(year):
                         tmdb_code = t['id']
                         break
-                    else:
-                        continue
-                        
-            meta_cache[keyword] = tmdb_code
-            return tmdb_code
-
-        elif tmdb_search['results'] == [] and SiteUtil.is_include_hangul(keyword):
+                if not tmdb_code:
+                    tmdb_code = tmdb_search['results'][0]['id']
+        else:
             try:
-                if cls.search_watch(keyword=keyword, year=year)['ret'] == 'success':
-                    cls.search(keyword=keyword, year=year)
+                watch_result = cls.search_watch(keyword=keyword, year=year)
+                if watch_result.get('ret') == 'success':
+                    new_keyword = watch_result.get('updated_keyword', keyword)
+                    return cls.search(new_keyword, year, meta_cache, searched_keywords) 
             except:
                 pass
+            tmdb_code = ''  
     
+        meta_cache[keyword] = tmdb_code
+        return tmdb_code
+
     @classmethod
     def search_watcha(cls, keyword, year=None):
         ret = {'ret': 'empty', 'data': None}
