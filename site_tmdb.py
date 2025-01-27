cd7bf46a02dd7efcee9f56e9828e96d3591d5256
kihyyo <83176214+kihyyo@users.noreply.github.com>
2025-01-27 22:37:01 +0900

    Update site_tmdb.py

--- site_tmdb.py
+++ site_tmdb.py
@@ -23,57 +23,77 @@ class tmdb(object):
         return difflib.SequenceMatcher(a=cls.remove_special_char(seq1.lower()), b=cls.remove_special_char(seq2.lower())).ratio()
 
     @classmethod
-    def search(cls, keyword, year=None, meta_cache={}, searched_keywords=None):
+    def search(keyword, is_show, year=None, meta_cache={}):
         tmdb.API_KEY = 'f090bb54758cabf231fb605d3e3e0468'
-
-        if searched_keywords is None:
-            searched_keywords = set()
-        
-        if keyword in searched_keywords:
-            return '' 
-
-        searched_keywords.add(keyword)
     
-        if keyword in meta_cache:
-            return meta_cache[keyword]
-
-        language = 'ko' if SiteUtil.is_include_hangul(keyword) else 'en'
-        tmdb_search = tmdbsimple.Search().tv(query=keyword, language=language, include_adult=True)
+        cache_key = (keyword, year)
     
+        if cache_key in meta_cache:
+            return meta_cache[cache_key]
+    
+        lang = 'ko' if isHangul(keyword) else 'en'
+        if is_show:
+            tmdb_search = tmdb.Search().tv(query=keyword, language=lang, include_adult=True)
+        else:
+            tmdb_search = tmdb.Search().movie(query=keyword, language=lang, include_adult=True)
+    
+        tmdb_code = ''
         if tmdb_search['results']:
+            key_name = 'name' if is_show else 'title'
+            key_original = 'original_name' if is_show else 'original_title'
+            results = tmdb_search['results']
+    
             if year is None:
-                score_list = [
-                    max(cls.similar(t['name'], keyword), cls.similar(t['original_name'], keyword))
-                    for t in tmdb_search['results']
-                ]
-                if max(score_list) > 0.7:
-                    tmdb_code = tmdb_search['results'][score_list.index(max(score_list))]['id']
+                score_list = []
+                for t in results:
+                    score_list.append(
+                        max(similar(t.get(key_name, ''), keyword), similar(t.get(key_original, ''), keyword)))
+                max_score = max(score_list)
+                if max_score > 0.85:
+                    max_indices = [i for i, score in enumerate(score_list) if score == max_score]
+                    if len(max_indices) == 1:
+                        tmdb_code = results[max_indices[0]]['id']
+                    else:
+                        recent_result = None
+                        for index in max_indices:
+                            t = results[index]
+                            try:
+                                tmdb_year = int(t['first_air_date'].split('-')[0]) if is_show else int(t['release_date'].split('-')[0])
+                            except:
+                                tmdb_year = 1900
+                            if recent_result is None or tmdb_year > recent_result[1]:
+                                recent_result = (t['id'], tmdb_year)
+                        tmdb_code = recent_result[0]
                 else:
-                    tmdb_code = tmdb_search['results'][0]['id']
+                    tmdb_code = results[0]['id']
             else:
-                tmdb_code = ''
-                for t in tmdb_search['results']:
+                results_with_similarity = []
+                for t in results:
                     try:
-                        tmdb_year = int(t['first_air_date'].split('-')[0])
+                        tmdb_year = int(t['first_air_date'].split('-')[0]) if is_show else int(t['release_date'].split('-')[0])
                     except:
                         tmdb_year = 1900
-    
-                    if tmdb_year == int(year):
-                        tmdb_code = t['id']
+                    similarity_score = max(similar(t.get(key_name, ''), keyword), similar(t.get(key_original, ''), keyword))
+                    results_with_similarity.append((t['id'], tmdb_year, similarity_score))
+                results_with_similarity.sort(key=lambda x: x[2], reverse=True)
+                for tmdb_id, tmdb_year, similarity_score in results_with_similarity:
+                    if similarity_score >= 0.85 and tmdb_year == int(year):
+                        tmdb_code = tmdb_id
                         break
-                if not tmdb_code:
-                    tmdb_code = tmdb_search['results'][0]['id']
-        else:
-            try:
-                watch_result = cls.search_watch(keyword=keyword, year=year)
-                if watch_result.get('ret') == 'success':
-                    new_keyword = watch_result.get('updated_keyword', keyword)
-                    return cls.search(new_keyword, year, meta_cache, searched_keywords) 
-            except:
-                pass
-            tmdb_code = ''  
     
-        meta_cache[keyword] = tmdb_code
+                if not tmdb_code:
+                    for tmdb_id, tmdb_year, similarity_score in results_with_similarity:
+                        if similarity_score >= 0.85 and abs(tmdb_year - int(year)) == 1:
+                            tmdb_code = tmdb_id
+                            break
+    
+                if not tmdb_code:
+                    for tmdb_id, tmdb_year, similarity_score in results_with_similarity:
+                        if similarity_score >= 0.85 and abs(tmdb_year - int(year)) == 2:
+                            tmdb_code = tmdb_id
+                            break
+    
+        meta_cache[cache_key] = tmdb_code
         return tmdb_code
 
     @classmethod
